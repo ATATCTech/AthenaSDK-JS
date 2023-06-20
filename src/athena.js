fb3bcd0dfbce6288bc8456779fcf4c418df0634d
ATATC <futerry@outlook.com>
2023-06-20 18:44:20 -0400

    Version `1.0.0`.

--- src/athena.js
+++ src/athena.js
@@ -19,6 +19,14 @@ export class Athena {
             handler(r.data.status, r.data.message);
         }).catch(except == null ? this.defaultExcept : except);
     }
+    async _get(ep, pathVariables = []) {
+        const r = await axios.get(this.getEndpoint(ep) + "/" + pathVariables.join("/"));
+        return [r.data.satus, r.data.message];
+    }
+    async _post(ep, data) {
+        const r = await axios.post(this.getEndpoint(ep), data);
+        return [r.data.status, r.data.message];
+    }
 }
 export function useAthena(baseUrl = "https://athena2.atatctech.com", defaultExcept = () => { }) {
     return new Athena(baseUrl, defaultExcept);
--- src/athena.ts
+++ src/athena.ts
@@ -24,6 +24,16 @@ export class Athena {
             handler(r.data.status, r.data.message);
         }).catch(except == null ? this.defaultExcept : except);
     }
+
+    public async _get(ep: string, pathVariables: string[] = []): Promise<[number, string]> {
+        const r = await axios.get(this.getEndpoint(ep) + "/" + pathVariables.join("/"));
+        return [r.data.satus, r.data.message];
+    }
+
+    public async _post(ep: string, data: any): Promise<[number, string]> {
+        const r = await axios.post(this.getEndpoint(ep), data);
+        return [r.data.status, r.data.message];
+    }
 }
 
 export function useAthena(
